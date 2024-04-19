89460f54aec4da5666bf85c26040ec9beef364a9
Student RM4-3073 11 <Student RM4-3073 11@RDS-LAB14>
2024-04-19 18:03:37 +0200

    Dodaj pliki projektów.

---
+++ Controllers/HistoriaController.cs
@@ -0,0 +1,26 @@
+﻿using BLL.DTOModels;
+using BLL.ServiceInterfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BazyDanych.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HistoriaController : ControllerBase
+    {
+        private readonly IHistoriaService _historiaService;
+
+        public HistoriaController(IHistoriaService historiaService)
+        {
+            _historiaService = historiaService;
+        }
+
+        [HttpGet]
+        public IActionResult GetHistorie([FromQuery] int pageNumber, [FromQuery] int pageSize)
+        {
+            var historie = _historiaService.GetHistoriaPage(pageNumber, pageSize);
+            return Ok(historie);
+        }
+    }
+}
---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,46 @@
+﻿using BLL.DTOModels;
+using BLL.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BazyDanych.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IStudentService _studentService;
+
+        public StudentsController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        [HttpPost]
+        public IActionResult AddStudent([FromBody] StudentDTO studentDto)
+        {
+            _studentService.AddStudent(studentDto);
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            var student = _studentService.GetStudent(id);
+            return student != null ? Ok(student) : NotFound();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateStudent([FromBody] StudentDTO studentDto)
+        {
+            _studentService.UpdateStudent(studentDto);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            _studentService.DeleteStudent(id);
+            return Ok();
+        }
+    }
+}
